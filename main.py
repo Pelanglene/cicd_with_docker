36e9ed18e3d5edef5aa1d3be0dc28b0c425ab7b5
Vladislav <anokhov04@mail.ru>
2023-04-16 16:14:15 +0000

    update

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import time 
 import logging 
+import os
+import subprocess
 
 if __name__ == '__main__':
     times = 0
+    os.system("pwd")
     while True:
-        with open("test.txt", "a") as file:
+        with open("data/test" + str(time.time()) + ".txt", "a") as file:
             file.write(str(int(time.time())))
         print(time.time())
         time.sleep(3)
         times += 1
         if times == 3:
             break
-
